50afe9b95a59c5367bcf3d4b38880dcf2d9fbe27
andychicken <83952965+andychicken@users.noreply.github.com>
2021-11-14 11:35:55 +0700

    indexs

--- src/models/user.js
+++ src/models/user.js
@@ -1,23 +1,21 @@
 const { Schema,model } = require('mongoose')
 const mongoose = require('mongoose')
-const animalSchema = new Schema({ name: String, type: String });
-
-  // Assign a function to the "statics" object of our animalSchema
-animalSchema.query.byName = function(name) {
-    return this.where({ name: new RegExp(name, 'i') })
-  };
-
+const animalSchema = new Schema({
+  name: String,
+  type: String,
+  tags: { type: [String], index: true } // field level
+});
+animalSchema.set('autoIndex', true)
+animalSchema.index({ name: 1, type: -1 }); 
 const Animal = mongoose.model('Animal', animalSchema);
 
-
-const dog = new Animal({ name: 'fido' });
-  
-  (async ()=>{
-      //await Animal.deleteMany({})
+const dog = new Animal({ name: 'fido' , type :"dog",tags:["a","b"]});
+ (async ()=>{
+      await Animal.deleteMany({})
       
       await Animal.create(dog)
 
-      const ex = await Animal.find().byName('fido')
+      const ex = await Animal.find({})
       console.log(ex)
   })()
 
